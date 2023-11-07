f12d009641153114e725b5c502804a436a6233a3
Vanessa R. <vanessasweden94@outlook.com>
2023-11-07 22:39:44 +0100

    Created a manager for the tasks
    Here I created also a menu through which the user can pick an option by
    entering a number between 1 and 4.

--- interact.js
+++ interact.js
@@ -3,3 +3,13 @@ import { TodoList } from './todolist.js';
 
 const prompt = PromptSync({ sigint: true });
 
+
+const myTodoList = new TodoList();
+
+console.log('1.Add task');
+console.log('2.Remove task');
+console.log('3.Complete task');
+console.log('4.Display tasks');
+
+let choice = prompt("Enter your choice from 1 to 4: ");
+
