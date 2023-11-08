b60518b41d0d94310cef6b6dceed3ce02a7290f1
Vanessa R. <vanessasweden94@outlook.com>
2023-11-08 14:11:36 +0100

    Implemented user's input choices
    Here the user will be able to choose what to do.
    
    -add task
    -remove task
    -complete task
    -view TodoList

--- interact.js
+++ interact.js
@@ -13,3 +13,18 @@ console.log('4.Display tasks');
 
 let choice = prompt("Enter your choice from 1 to 4: ");
 
+if (choice === '1') {
+  myTodoList.addTask(prompt('Add task: '));
+} 
+else if (choice === '2') {
+  myTodoList.removeTask(prompt('Remove task: '));
+}
+else if (choice === '3') {
+  myTodoList.completeTask(prompt('Complete task: '));
+}
+else if (choice === '4') {
+  displayTasks(myTodoList);
+}
+else {
+  console.log('HAKUNA MATATA :)')
+}
